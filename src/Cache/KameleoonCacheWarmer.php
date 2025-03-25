f1fb2503dea21c35898a3255b71a04ac0c735c94
Surdy <surdy@protonmail.com>
2025-03-25 12:57:34 +0100

    LW-29772 Added cache warmuper to create working dir on cache warmup instead of on factory call

---
+++ src/Cache/KameleoonCacheWarmer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Lingoda\KameleoonBundle\Cache;
+
+use Lingoda\KameleoonBundle\Kameleoon\KameleoonConfig;
+use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
+
+class KameleoonCacheWarmer implements CacheWarmerInterface
+{
+    public function __construct(
+        private readonly KameleoonConfig $config
+    )
+    {
+    }
+
+    public function isOptional(): bool
+    {
+        return false;
+    }
+
+    public function warmUp(string $cacheDir): array
+    {
+        $this->warmUpWorkingDir();
+
+        return [];
+    }
+
+    private function warmUpWorkingDir(): void
+    {
+        if (!is_dir($this->config->getConfig()->getKameleoonWorkDir())) {
+            @mkdir($this->config->getConfig()->getKameleoonWorkDir(), 0755, true);
+        }
+    }
+}
---
+++ src/Kameleoon/KameleoonClientFactoryWrapper.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lingoda\KameleoonBundle\Kameleoon;
+
+use Kameleoon\KameleoonClient;
+use Kameleoon\KameleoonClientFactory as KameleoonSDKClientFactory;
+
+class KameleoonClientFactoryWrapper
+{
+    public static function createWithConfig(KameleoonConfig $config): KameleoonClient
+    {
+        return KameleoonSDKClientFactory::createWithConfig(
+            $config->getKameleoonSiteCode(),
+            $config->getConfig()
+        );
+    }
+}
