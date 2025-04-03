22daa03e9042bc5e8f7e0cc68d30431825f6793e
Surdy <surdy@protonmail.com>
2025-04-03 17:04:50 +0200

    LW-30295 fopen() permission denied fix

--- src/Kameleoon/KameleoonConfig.php
+++ src/Kameleoon/KameleoonConfig.php
@@ -47,6 +47,7 @@ class KameleoonConfig
         return $this->kameleoonSiteCode;
     }
 
+    /* @phpstan-ignore-next-line */
     private function getCookieOptions(): CookieOptions
     {
         return KameleoonClientConfig::createCookieOptions(
